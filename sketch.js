93416aca2b13472355a0efa3f852cdcfa08cfa5c
zmokito <145008514+zmokito@users.noreply.github.com>
2023-09-28 12:46:53 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let zBereich;
+
+
+function setup() {
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0);
+  zBereich.style('z-Index','-1');
+}
+
+
+
+function draw() {
+  noStroke();
+  fill(255, 50, 0);
+    circle(mouseX, mouseY, 24);
+}
+
+
+// function mousePressed() {
+//   background(0);
+// }
